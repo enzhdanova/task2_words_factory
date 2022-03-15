fc32a692a6e1a1463bc109c86acf0ea127908797
enzhdanova <listochek.katerinskiy@gmail.com>
2022-03-15 19:53:03 +0700

    add ViewBinding, ann LiveData

--- app/src/main/java/com/example/task/wordsfactory/ui/view/SignUpScreenActivity.kt
+++ app/src/main/java/com/example/task/wordsfactory/ui/view/SignUpScreenActivity.kt
@@ -9,6 +9,7 @@ import android.widget.Button
 import android.widget.EditText
 import androidx.activity.viewModels
 import com.example.task.wordsfactory.R
+import com.example.task.wordsfactory.databinding.ActivitySignUpScreenBinding
 import com.example.task.wordsfactory.ui.viewmodel.SignUpViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -22,75 +23,25 @@ import dagger.hilt.android.AndroidEntryPoint
 class SignUpScreenActivity : AppCompatActivity() {
 
     private val viewModel by viewModels<SignUpViewModel>()
+    private var binding: ActivitySignUpScreenBinding? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sign_up_screen)
+        binding = ActivitySignUpScreenBinding.inflate(layoutInflater)
+        val view = binding?.root
 
-        val nameEditText = findViewById<EditText>(R.id.activity_sign_up_name_edittext)
-        val emailEditText = findViewById<EditText>(R.id.activity_sign_up_e_mail_edittext)
-        val passwordEditText = findViewById<EditText>(R.id.activity_sign_up_password_edittext)
-        val signUpButton = findViewById<Button>(R.id.activity_sign_up_button)
+        setContentView(view)
 
-        nameEditText.addTextChangedListener(textWatcherName)
-        emailEditText.addTextChangedListener(textWatcherEmail)
-        passwordEditText.addTextChangedListener(textWatcherPassword)
-        signUpButton.setOnClickListener(buttonSignUpOnClick)
-
-
-      /*  val authRepositoryImpl: AuthRepositoryImpl = AuthRepositoryImpl(application)
-        authRepositoryImpl.login("1","2","3")
-
-        val sP = application.getSharedPreferences("preference", Context.MODE_PRIVATE)
-        val tmp = sP.getString("example", "0")
-        System.out.println("getSharedPreferences $tmp")
-*/
-
-       /* lifecycleScope.launch {
-            repeatOnLifecycle(Lifecycle.State.STARTED){
-                viewModel.uiState.collect {
-                }
-
-            }
-        }*/
-    }
-
-    private val textWatcherName = object : TextWatcher{
-        override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
-
-        override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
-            viewModel.editTextNameChanged(p0.toString())
-        }
-
-        override fun afterTextChanged(p0: Editable?) {}
-    }
-
-    private val textWatcherEmail = object : TextWatcher{
-        override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
-
-        override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
-            viewModel.editTextEmailChanged(p0.toString())
-        }
-
-        override fun afterTextChanged(p0: Editable?) {}
-    }
-
-    private val textWatcherPassword = object : TextWatcher{
-        override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
-
-        override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
-            viewModel.editTextPasswordChanged(p0.toString())
-        }
-
-        override fun afterTextChanged(p0: Editable?) {}
+        binding?.activitySignUpButton?.setOnClickListener(buttonSignUpOnClick)
     }
 
     private val buttonSignUpOnClick = View.OnClickListener {
+        viewModel.editTextNameChanged(binding?.activitySignUpNameEdittext?.text.toString())
+        viewModel.editTextEmailChanged(binding?.activitySignUpEMailEdittext?.text.toString())
+        viewModel.editTextPasswordChanged((binding?.activitySignUpPasswordEdittext?.text.toString()))
         viewModel.login()
         val userLogin = viewModel.getUser()
         System.out.println("getSharedPreferences $userLogin")
     }
-
-
 
 }
--- app/src/main/java/com/example/task/wordsfactory/ui/viewmodel/SignUpViewModel.kt
+++ app/src/main/java/com/example/task/wordsfactory/ui/viewmodel/SignUpViewModel.kt
@@ -1,5 +1,7 @@
 package com.example.task.wordsfactory.ui.viewmodel
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.task.wordsfactory.data.AuthRepositoryImpl
 import com.example.task.wordsfactory.data.model.UserLogin
@@ -15,43 +17,36 @@ import javax.inject.Inject
 class SignUpViewModel @Inject constructor(
     private val authRepository: AuthRepository
 ) : ViewModel() {
-    private val _uiState = MutableStateFlow(SignUpUiState())
-    val uiState: StateFlow<SignUpUiState> = _uiState.asStateFlow()
+
+    private val  _uiState = MutableLiveData<SignUpUiState>()
+    val uiState : LiveData<SignUpUiState>
+        get() = _uiState
 
     fun editTextNameChanged(newText: String) {
-        _uiState.update {
-            it.copy(name = newText)
-        }
+        _uiState.value = _uiState.value?.copy(name = newText) ?: SignUpUiState(name = newText)
     }
 
     fun editTextEmailChanged(newText: String) {
-        _uiState.update {
-            it.copy(e_mail = newText)
-        }
+        _uiState.value = _uiState.value?.copy(e_mail = newText) ?: SignUpUiState(e_mail = newText)
+        println(uiState.value)
     }
 
     fun editTextPasswordChanged(newText: String) {
-        _uiState.update {
-            it.copy(password = newText)
-        }
+        _uiState.value = _uiState.value?.copy(password = newText) ?: SignUpUiState(password = newText)
     }
 
     fun login() {
         val user = uiState.value
         System.out.println(user)
-        if (user.name == "" || user.e_mail == "" || user.password == "") {
+        if (user?.name == "" || user?.e_mail == "" || user?.password == "") {
             // TODO: тут надо вывести сообщение об ошибке, диалог и все такое
             // TODO: Проверка корректного ввода всех полей
         } else {
-            authRepository.login(user.name, user.e_mail, user.password)
-
-
+            user?.let { authRepository.login(it.name, it.e_mail, it.password) }
         }
     }
 
     fun getUser(): UserLogin {
         return authRepository.getUser()
     }
-
-
 }
