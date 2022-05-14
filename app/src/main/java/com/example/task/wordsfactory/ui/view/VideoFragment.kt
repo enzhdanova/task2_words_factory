a38353872ef4efca3d4edd749de9f35c5ede5f92
enzhdanova <listochek.katerinskiy@gmail.com>
2022-05-14 13:04:44 +0700

    add onBackPressed

--- app/src/main/java/com/example/task/wordsfactory/ui/view/VideoFragment.kt
+++ app/src/main/java/com/example/task/wordsfactory/ui/view/VideoFragment.kt
@@ -1,10 +1,12 @@
 package com.example.task.wordsfactory.ui.view
 
+import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.OnBackPressedCallback
 import com.example.task.wordsfactory.BuildConfig
 import com.example.task.wordsfactory.databinding.FragmentVideoBinding
 import com.example.task.wordsfactory.ui.utils.VideoWebViewClient
@@ -34,7 +36,17 @@ class VideoFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         binding?.webview?.loadUrl(BuildConfig.VIDEO_URL)
         binding?.webview?.webViewClient = VideoWebViewClient()
-    }
 
+        val callback: OnBackPressedCallback = object : OnBackPressedCallback(true){
+
+            override fun handleOnBackPressed() {
+                if (binding?.webview?.canGoBack() == true) {
+                    binding?.webview?.goBack()
+                }
+            }
+        }
+
+        requireActivity().onBackPressedDispatcher.addCallback(viewLifecycleOwner, callback)
+    }
 
 }
