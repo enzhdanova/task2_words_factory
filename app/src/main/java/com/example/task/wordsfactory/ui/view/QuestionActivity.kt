f8dffdd31c8b1e9425ac3df8d0ce774e77b57251
enzhdanova <listochek.katerinskiy@gmail.com>
2022-06-21 14:44:34 +0700

    add question layout

---
+++ app/src/main/java/com/example/task/wordsfactory/ui/view/QuestionActivity.kt
@@ -0,0 +1,12 @@
+package com.example.task.wordsfactory.ui.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.task.wordsfactory.R
+
+class QuestionActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_question)
+    }
+}
