eca8a2b5c83ef908cbb4f052d5369aeba462123e
enzhdanova <listochek.katerinskiy@gmail.com>
2022-06-22 14:12:03 +0700

    add plug words for answer in question

--- app/src/main/java/com/example/task/wordsfactory/data/DictionaryRepositoryImpl.kt
+++ app/src/main/java/com/example/task/wordsfactory/data/DictionaryRepositoryImpl.kt
@@ -2,7 +2,6 @@ package com.example.task.wordsfactory.data
 
 import com.example.task.wordsfactory.data.datasource.LocalDataSource
 import com.example.task.wordsfactory.data.datasource.RemoteDataSource
-import com.example.task.wordsfactory.data.model.Meaning
 import com.example.task.wordsfactory.data.model.Question
 import com.example.task.wordsfactory.data.model.Word
 import com.example.task.wordsfactory.ui.DictionaryRepository
@@ -14,6 +13,10 @@ class DictionaryRepositoryImpl @Inject constructor(
     private val remoteDataSource: RemoteDataSource,
     private val localDataSource: LocalDataSource
 ) : DictionaryRepository {
+
+    companion object {
+        private const val COUNT_ANSWER_IN_QUESTION = 3
+    }
 
     override suspend fun getWord(searchWord: String): Result<Word> {
         val result = remoteDataSource.getWord(searchWord)
@@ -51,17 +54,24 @@ class DictionaryRepositoryImpl @Inject constructor(
             .getWrongWordsForQuestion(rightWord.word)
             .getOrNull()
 
-        //TODO: Решить как добавить ответы заглушки
-        val resultAnswers = mutableListOf(rightWord.word)
-        if (answers != null) {
-            resultAnswers.addAll(answers)
-            resultAnswers.shuffle()
-        }
-        return  if (answers == null || meaning == null) {
+        return if (answers == null || meaning == null) {
             Result.failure(NullPointerException())
         } else {
+            val resultAnswers = shuffleAnswers(rightWord.word, answers)
             Result.success(Question(nowQuestion = meaning, answers = resultAnswers))
         }
     }
 
+    private fun getPlugAnswers(countAnswer: Int): List<String> =
+        WordsForPlugAnswer.plugAnswers.shuffled().take(countAnswer)
+
+    private fun shuffleAnswers(rightWord: String, answers: List<String>): List<String> {
+        val resultAnswers = mutableListOf(rightWord)
+        if (answers.size < COUNT_ANSWER_IN_QUESTION) {
+            resultAnswers.addAll(getPlugAnswers(COUNT_ANSWER_IN_QUESTION - answers.size))
+        }
+        resultAnswers.addAll(answers)
+        resultAnswers.shuffle()
+        return resultAnswers
+    }
 }
