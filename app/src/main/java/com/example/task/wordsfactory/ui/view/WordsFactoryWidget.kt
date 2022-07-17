8344baacf25d6766c9c7c5f6d183167298d944f8
enzhdanova <listochek.katerinskiy@gmail.com>
2022-07-17 10:09:12 +0300

    add count words in dictionary in widget

--- app/src/main/java/com/example/task/wordsfactory/ui/view/WordsFactoryWidget.kt
+++ app/src/main/java/com/example/task/wordsfactory/ui/view/WordsFactoryWidget.kt
@@ -5,41 +5,51 @@ import android.appwidget.AppWidgetProvider
 import android.content.Context
 import android.widget.RemoteViews
 import com.example.task.wordsfactory.R
+import com.example.task.wordsfactory.data.DictionaryRepositoryImpl
+import com.example.task.wordsfactory.data.datasource.LocalDataSource
+import com.example.task.wordsfactory.data.datasource.RemoteDataSource
+import com.example.task.wordsfactory.database.AppDatabase
+import com.example.task.wordsfactory.database.dao.DictionaryDao
+import com.example.task.wordsfactory.ui.DictionaryRepository
+import dagger.hilt.android.AndroidEntryPoint
+import dagger.hilt.android.HiltAndroidApp
+import dagger.hilt.android.scopes.ServiceScoped
+import dagger.hilt.android.scopes.ViewScoped
+import kotlinx.coroutines.*
+import javax.inject.Inject
+import kotlin.coroutines.coroutineContext
 
-/**
- * Implementation of App Widget functionality.
- */
+@AndroidEntryPoint
 class WordsFactoryWidget : AppWidgetProvider() {
+    @Inject
+    lateinit var appDatabase: AppDatabase
+
     override fun onUpdate(
         context: Context,
         appWidgetManager: AppWidgetManager,
         appWidgetIds: IntArray
     ) {
-        // There may be multiple widgets active, so update all of them
         for (appWidgetId in appWidgetIds) {
-            updateAppWidget(context, appWidgetManager, appWidgetId)
+            updateAppWidget(appDatabase, context, appWidgetManager, appWidgetId)
         }
     }
 
-    override fun onEnabled(context: Context) {
-        // Enter relevant functionality for when the first widget is created
-    }
-
-    override fun onDisabled(context: Context) {
-        // Enter relevant functionality for when the last widget is disabled
-    }
 }
 
 internal fun updateAppWidget(
+    appDatabase: AppDatabase,
     context: Context,
     appWidgetManager: AppWidgetManager,
     appWidgetId: Int
 ) {
-  //  val widgetText = context.getString(R.string.appwidget_text)
-    // Construct the RemoteViews object
     val views = RemoteViews(context.packageName, R.layout.words_factory_widget)
-   // views.setTextViewText(R.id.appwidget_text, widgetText)
 
-    // Instruct the widget manager to update the widget
-    appWidgetManager.updateAppWidget(appWidgetId, views)
+    CoroutineScope(Dispatchers.IO).launch {
+        val result = appDatabase.dictionaryDao().getCountWords().toString()
+
+        views.setTextViewText(R.id.count_words_in_dictionary_value, result + " words")
+        views.setTextColor(R.id.count_words_in_dictionary_value, context.getColor(R.color.teal_700))
+        appWidgetManager.updateAppWidget(appWidgetId, views)
+    }
+
 }
